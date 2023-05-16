5c1b1261c368a4f089320f59dfc8ec606cc7fec0
DnsYOUdnk <dns.yudenka@gmail.com>
2023-05-17 01:50:03 +0300

    feat: start supervisor

--- echoserver.js
+++ echoserver.js
@@ -17,8 +17,8 @@ const server = new http.Server(function(req, res){
     res.end(urlParsed.query.message)
   } else {
     res.statusCode = 404;
-    res.end('Page not found')
+    res.end('Page not found');
   }
 })
 
-server.listen(1337, '127.0.0.1');
+server.listen(1337);
