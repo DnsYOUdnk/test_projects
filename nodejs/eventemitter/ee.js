ad8c242feb0eb5cabfaa618d50cdd5a06bfbb860
DnsYOUdnk <dns.yudenka@gmail.com>
2023-05-17 00:00:56 +0300

    feat: get all event listener

--- nodejs/eventemitter/ee.js
+++ nodejs/eventemitter/ee.js
@@ -1,9 +1,14 @@
 const EventEmitter = require("events").EventEmitter;
 
 const server = new EventEmitter;
+let count = 0;
 
 server.on('request', function(request) {
   request.approved = true;
+})
+
+server.on('request', function(request) {
+  request.counter = count++;
 })
 
 server.on('request', function(request) {
@@ -12,3 +17,5 @@ server.on('request', function(request) {
 
 server.emit('request', {from: 'Клиент'});
 server.emit('request', {from: 'Ещё Клиент'});
+
+console.log(server.listeners('request'))
