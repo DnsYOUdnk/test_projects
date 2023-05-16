8ca4c1f5becf38680a4ada1c81142b4c60fa63bb
DnsYOUdnk <dns.yudenka@gmail.com>
2023-05-17 01:28:51 +0300

    feat: add headers

--- echoserver.js
+++ echoserver.js
@@ -2,12 +2,18 @@ const http = require('http');
 const url = require('url')
 
 const server = new http.Server(function(req, res){
+  console.log(req.headers, req.url);
+
+
+
   console.log(req.method, req.url);
 
   const urlParsed = url.parse(req.url, true);
   console.log(urlParsed);
 
   if(urlParsed.pathname === '/echo' && urlParsed.query.message) {
+    // res.statusCode = 200;
+    res.setHeader('Cache-control', 'no-cache, no-store, must-revalidate' )
     res.end(urlParsed.query.message)
   } else {
     res.statusCode = 404;
