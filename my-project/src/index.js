d36173348d4f937028a4c4dca2140cb5339dd0f0
DnsYOUdnk <dns.yudenka@gmail.com>
2023-05-19 01:41:26 +0300

    feat: add new config for entry and output point

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -4,4 +4,25 @@ function component(text) {
   return element;
 }
 
+
+const list = document.createElement('ol');
+
+let count = 0;
+let int;
+
+function addList(num) {
+  if(count > 10) {
+    clearInterval(int);
+    return;
+  }
+  const li = document.createElement('li');
+  const color = ['pink', 'green', 'orange', 'purple', 'blue', 'red', 'darkred', 'black', 'violet', 'darkblue']
+  li.style.color = color[num - 1];
+  li.textContent = `number - ${num}`;
+  list.append(li);
+}
+
+int = setInterval(() => addList(++count), 1000);
+
 document.body.prepend(component('Проект собран с помощью Webpack 5.83.1'));
+document.body.append(list);
