270c8dd04cdd27384a306765b8dc1b8c5a2af57f
DnsYOUdnk <dns.yudenka@gmail.com>
2023-05-19 19:07:45 +0300

    feat: create counter

--- my-project/src/components/app.js
+++ my-project/src/components/app.js
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  
   render() {
     return (
-      <h1 className="h1">
-        Приложение на React
-      </h1>
+      <>
+        <h1 className="h1">
+          Приложение на React
+        </h1>
+        <div>{this.state.count}</div>
+        <button onClick={() => this.setState({count: ++this.state.count})}>Click me</button>
+      </>
     )
   }
 }
